6605929b9c0cd069ab836bfd5dc1a4750a61285d
Bastiani <rafacdb@gmail.com>
2017-11-22 15:02:52 -0200

    layout, error in react-native-camera

--- App.js
+++ App.js
@@ -1,19 +1,101 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Text, TextInput, StyleSheet, View, Image, Button, TouchableOpacity } from 'react-native';
+import axios from 'axios';
+import QRCodeScanner from 'react-native-qrcode-scanner';
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
+  header: {
     alignItems: 'center',
-    justifyContent: 'center',
+    height: 50,
+    backgroundColor: 'powderblue',
+    paddingTop: 10,
+  },
+  imgTop: {
+    resizeMode: 'stretch',
+    width: 100,
+    height: 35,
+  },
+  initialForm: {
+    padding: 25,
+  },
+  buttonText: {
+    fontSize: 21,
+    color: 'rgb(0,122,255)',
+  },
+  buttonTouchable: {
+    padding: 16,
   },
 });
 
-const App = () => (
-  <View style={styles.container}>
-    <Text>Rafael</Text>
-  </View>
-);
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { gistId: '', content: '', message: '' };
 
+    this.onShowGist = () => () => {
+      const { gistId } = this.state;
+      axios
+        .get(`https://api.github.com/gists/${gistId}`)
+        .then((response) => {
+          const { content } = response.files[0];
+          this.setState({ content });
+        })
+        .catch((e) => {
+          const { message } = e.message;
+          this.setState({ message });
+        });
+    };
+
+    this.readQRCode = (e) => {
+      this.setState({ gistId: e.data });
+    };
+  }
+
+  render() {
+    return (
+      <View>
+        <View>
+          <View style={styles.header}>
+            <Image style={styles.imgTop} source={require('./assets/logo_gist.png')} />
+          </View>
+          <View style={styles.initialForm}>
+            <Text value={this.state.message} />
+            <TextInput
+              style={{ height: 50 }}
+              placeholder="Gist URL"
+              onChangeText={gistId => this.setState({ gistId })}
+              value={this.state.gistId}
+            />
+            <QRCodeScanner
+              onRead={this.readQRCode(this)}
+              topContent={
+                <Text style={styles.centerText}>
+                  Go to <Text style={styles.textBold}>wikipedia.org/wiki/QR_code</Text> on your
+                  computer and scan the QR code.
+                </Text>
+              }
+              bottomContent={
+                <TouchableOpacity style={styles.buttonTouchable}>
+                  <Text style={styles.buttonText}>OK. Got it!</Text>
+                </TouchableOpacity>
+              }
+            />
+            <Button
+              onPress={() => this.onShowGist()}
+              title="Show Gist"
+              color="cadetblue"
+              accessibilityLabel="Show Gist"
+            />
+            <TextInput
+              style={{ height: 50 }}
+              placeholder="Gist Content"
+              value={this.state.content}
+              multiline
+            />
+          </View>
+        </View>
+      </View>
+    );
+  }
+}
 export default App;
